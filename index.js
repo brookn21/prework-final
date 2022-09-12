274cfebe40892ed40c8f3e955d3c29deb081d701
Brook Nega <brooknega21@gmail.com>
2022-09-11 21:18:52 -0400

    color changer in js

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("changeColor").onclick = 
+function(){
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let bgColor = "rgb("+ x + "," + y  + "," + z  + ")";
+    document.body.style.background = bgColor;
+}
